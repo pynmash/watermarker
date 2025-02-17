d3a9bdf81759f36cbd36485dde3fd09f87247de9
Pynmash <157254317+pynmash@users.noreply.github.com>
2025-02-17 04:25:23 +0000

    Add heading

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ window = tk.Tk()
 window.title("Watermarker")
 window.minsize(width=500, height=100)
 
+
+# Set title heading
+title_label = tk.Label(text="Watermarker: A watermarking tool")
+title_label.grid(row=0, column=1)
+
 # This keeps the window open. Make sure it stays at the bottom of the file
 window.mainloop()
